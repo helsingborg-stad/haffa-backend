d872143a9e701d14f52df2f231e94d7bb8cbeb70
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-06-19 16:03:36 +0200

    improved developer experience by supporting default user

--- src/services.ts
+++ src/services.ts
@@ -1,17 +1,9 @@
-import { AuthorizationService, createAuthorizationServiceFromEnv } from '@helsingborg-stad/gdi-api-node/services/authorization-service'
+import { createAuthorizationServiceFromEnv } from '@helsingborg-stad/gdi-api-node/services/authorization-service'
 import { Services } from './types'
 import { createAdvertsRepositoryFromEnv } from './adverts'
-import { getEnv } from '@helsingborg-stad/gdi-api-node'
-
-const tryCreateFakeAuthorizationFromEnv = (): AuthorizationService | null => {
-	const user = getEnv('FAKE_AUTHORIZATION_USER', { fallback: '' })
-	return user ? ({
-		tryGetUserFromJwt: () => JSON.parse(user),
-	}) : null
-}
 
 const createServicesFromEnv = (): Services => ({
-	authorization: tryCreateFakeAuthorizationFromEnv() || createAuthorizationServiceFromEnv(),
+	authorization: createAuthorizationServiceFromEnv(),
 	adverts: createAdvertsRepositoryFromEnv(),
 })
 
