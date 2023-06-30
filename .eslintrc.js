fca140db0979cb8ff7e937eee97f2ee442caca65
MrSpex <mail@dannilsson.se>
2023-06-30 14:19:53 +0200

    fix: development settings

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    'import/prefer-default-export': 0,
+    // Joakim
+    // 'react/jsx-props-no-spreading': 0,
+    // 'no-promise-executor-return': 0,
+    // 'react-hooks/exhaustive-deps': 0,
+    // '@typescript-eslint/no-use-before-define': 0,
+    // '@typescript-eslint/no-shadow': 0,
+    // '@typescript-eslint/no-unused-vars': 0,
+    // 'import/no-cycle': 0,
+    // 'react/jsx-no-duplicate-props': 0,
+    // 'react/destructuring-assignment': 0,
+    // 'consistent-return': 0,
+    // 'react/no-array-index-key': 0,
+    // 'react/jsx-no-constructed-context-values': 0,
+    // 'no-void': 0,
+    // '@typescript-eslint/no-unused-expressions': 0,
+    // 'no-sequences': 0,
+  },
+}
