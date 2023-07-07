09e9fc408756d7f0942c1f145b07219e9394a371
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-07-07 17:28:25 +0200

    server start logging to please devops

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { createServicesFromEnv } from './services'
 import type { Services } from './types'
 
 const services: Services = createServicesFromEnv()
-
+const port = process.env.PORT || 4000
 /** Main entry point that start and runs web server */
 void createApp({
   services,
   validateResponse: false,
-}).start(process.env.PORT || 4000)
+})
+.start(process.env.PORT || 4000)
+.then(() => console.log(`Haffa backend running on port ${port}`))
