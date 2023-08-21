e43df3372a896874eabd98a5a9a23e43daa76e9c
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-08-21 13:40:07 +0200

    ✨ UserMapper for later user and role management

--- src/login/try-create-haffa-user.ts
+++
@@ -1,5 +0,0 @@
-import type { HaffaUser } from './types'
-import { validateHaffaUser } from './validate-haffa-user'
-
-export const tryCreateHaffaUser = (user: any): HaffaUser | null => validateHaffaUser(user)
-
