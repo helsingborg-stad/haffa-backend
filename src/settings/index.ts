62591b1e10b4803d8901e8b588049e523e6be062
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-08-23 14:32:56 +0200

    settings persisted on disk

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,14 +1,13 @@
 import { getEnv } from "@helsingborg-stad/gdi-api-node";
 import { createInMemorySettingsService } from "./memory/in-memory-settings";
 import type { SettingsService } from "./types";
+import { tryCreateFsSettingsServiceFromEnv } from "./fs";
 
 export {createInMemorySettingsService}
 
-export const createSettingsServiceFromEnv = (): SettingsService => createInMemorySettingsService({
-	superUser: getEnv('SUPER_USER', {fallback: ''}),
-	loginPolicies: [{
-		emailPattern: '.*@helsingborg.se',
-		roles: ['a'],
-		deny: false
-	}]
-})
+export const createSettingsServiceFromEnv = (): SettingsService => 
+	tryCreateFsSettingsServiceFromEnv() ||
+	createInMemorySettingsService({
+		superUser: getEnv('SUPER_USER', {fallback: ''}),
+		loginPolicies: []
+	})
