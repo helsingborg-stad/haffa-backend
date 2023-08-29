a078c9b66050c3c00d1da075cddc3bc288178b4b
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-08-29 13:50:41 +0200

    exposing git_revision.txt in response header x-git-rev

--- src/create-app.ts
+++ src/create-app.ts
@@ -10,6 +10,7 @@ import {
 import type { Services } from './types'
 import { graphQLModule } from './haffa/haffa-module'
 import { loginModule } from './login/login-module'
+import { gitRevisionModule } from './git-revision-module'
 
 /** Create fully packaged web application, given dependencies */
 export const createApp = ({
@@ -24,6 +25,7 @@ export const createApp = ({
     validateResponse,
   })
     // .use(webFrameworkModule())
+    .use(gitRevisionModule())
     .use(({ app }) => app.use(cors()))
     .use(({ app }) => app.use(bodyparser({ jsonLimit: '50mb' })))
     .use(swaggerModule({routePrefix: '/api/v1/haffa/swagger'}))
---
+++ src/git-revision-module.ts
@@ -0,0 +1,15 @@
+import type { ApplicationModule } from "@helsingborg-stad/gdi-api-node";
+import { readFile } from "fs/promises";
+import { join } from "path";
+
+// Small module that exposes contents of 'git_revision.txt' to a response header
+export const gitRevisionModule = (): ApplicationModule => {
+	const revision = readFile(join(process.cwd(), 'git_revision.txt'),{encoding: 'utf8'})
+		.then(text => (text||'').split('\n').map(v => v.trim()).filter(v => v)[0] || '')
+		.catch(() => (''))
+	return (({app}) => app.use(async (ctx, next) => {
+		const r = await revision
+		ctx.set('x-git-rev', r)
+		return next()
+	}))
+}
