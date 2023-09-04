aa5b4ba56ca9f10a7e4e2f6428a5f2ba9fdcc0e5
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-09-04 12:54:04 +0200

    gql schema tidy

--- src/login-policies/login-policies.gql.schema.ts
+++ src/login-policies/login-policies.gql.schema.ts
@@ -2,12 +2,10 @@ export const loginPoliciesGqlSchema = /* GraphQL */`
 
 type Query {
 	loginPolicies: [LoginPolicy]
-	categories: [Category]
 }
 
 type Mutation {
 	updateLoginPolicies(input: [LoginPolicyInput]!): [LoginPolicy]
-	updateCategories(input: [CategoryInput]!): [Category]
 }
 
 type LoginPolicy {
