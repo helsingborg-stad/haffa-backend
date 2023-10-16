5944b8436ef04c83a9a2a7cb11d43880f261723d
Petter Andersson <petter.andersson@helsingborg.se>
2023-10-16 09:46:13 +0200

    add access whitelist

--- src/options/options-gql-module.ts
+++ src/options/options-gql-module.ts
@@ -4,6 +4,7 @@ import type { Services } from '../types'
 import { optionsGqlSchema } from './options.gql.schema'
 import { normalizeRoles } from '../login'
 import { optionsAdapter } from './options-adapter'
+import { allowedCollections } from './options-whitelist'
 
 export const createOptionsGqlModule = ({
   settings,
@@ -14,7 +15,10 @@ export const createOptionsGqlModule = ({
       // https://www.graphql-tools.com/docs/resolvers
       options: async ({ ctx, args: { name } }) => {
         const { user } = ctx
-        if (!normalizeRoles(user?.roles).canEditTerms) {
+        if (
+          !normalizeRoles(user?.roles).canEditTerms ||
+          !allowedCollections.includes(name)
+        ) {
           ctx.throw(HttpStatusCodes.UNAUTHORIZED)
         }
         return optionsAdapter(settings).getOptions(name)
@@ -23,7 +27,10 @@ export const createOptionsGqlModule = ({
     Mutation: {
       updateOptions: async ({ ctx, args: { input, name } }) => {
         const { user } = ctx
-        if (!normalizeRoles(user?.roles).canEditTerms) {
+        if (
+          !normalizeRoles(user?.roles).canEditTerms ||
+          !allowedCollections.includes(name)
+        ) {
           ctx.throw(HttpStatusCodes.UNAUTHORIZED)
         }
         return optionsAdapter(settings).updateOptions(name, input)
---
+++ src/options/options-whitelist.ts
@@ -0,0 +1,1 @@
+export const allowedCollections = ['branding-options']
