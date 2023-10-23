2565b82339260f1b2cf5101dd1dad31b9dcf130e
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-10-23 10:48:11 +0200

    cookie authentication tests

---
+++ src/login/cookies/cookie-request-valildation.spec.ts
@@ -0,0 +1,115 @@
+import request from 'supertest'
+import HttpStatusCodes from 'http-status-codes'
+import type { ApplicationModule } from '@helsingborg-stad/gdi-api-node'
+import type { Services } from '../../types'
+import { requireHaffaUser } from '../require-haffa-user'
+import {
+  end2endTest,
+  type End2EndTestConfig,
+  type End2EndTestContext,
+} from '../../test-utils'
+import type { LoginPolicy } from '../../login-policies/types'
+
+describe('user access validation', () => {
+  interface TestCase {
+    givenCookieUser: string | null
+    givenLoginPolicies: Partial<LoginPolicy>[]
+    expectResponse: Partial<request.Response>
+  }
+  const TestCases: [string, TestCase][] = [
+    [
+      'accepts token in cookie',
+      {
+        givenCookieUser: 'test@user.com',
+        givenLoginPolicies: [
+          {
+            emailPattern: '*@user.com',
+            roles: ['canReserveAdverts', 'canCollectAdverts'],
+          },
+        ],
+        expectResponse: {
+          status: HttpStatusCodes.OK,
+          body: { id: 'test@user.com', roles: {} },
+        },
+      },
+    ],
+    [
+      'token in cookie can be denied by login policies',
+      {
+        givenCookieUser: 'test@user.com',
+        givenLoginPolicies: [
+          {
+            emailPattern: '*@user.com',
+            roles: [],
+            deny: true,
+          },
+        ],
+        expectResponse: {
+          status: HttpStatusCodes.UNAUTHORIZED,
+        },
+      },
+    ],
+    [
+      'token in cookie must match login policies',
+      {
+        givenCookieUser: 'test@unknown-user.com',
+        givenLoginPolicies: [
+          {
+            emailPattern: '*@user.com',
+            roles: [],
+            deny: true,
+          },
+        ],
+        expectResponse: {
+          status: HttpStatusCodes.UNAUTHORIZED,
+        },
+      },
+    ],
+  ]
+
+  it.each(TestCases)(
+    `%s`,
+    async (
+      description: string,
+      { givenCookieUser, givenLoginPolicies, expectResponse }: TestCase
+    ) =>
+      end2endTest(
+        createTestConfig(),
+        async ({ server, services: { tokens }, loginPolicies }) => {
+          await loginPolicies.updateLoginPolicies(givenLoginPolicies)
+
+          const token = givenCookieUser
+            ? tokens.sign({ id: givenCookieUser })
+            : ''
+
+          const response = await makeEchoUserRequest(token, server)
+          expect(response).toMatchObject(expectResponse)
+        }
+      )
+  )
+
+  const createUserEchoModule =
+    (services: Services): ApplicationModule =>
+    ({ router }) =>
+      router.get(
+        '/echo-user',
+        requireHaffaUser(services.userMapper, ctx => {
+          ctx.body = ctx.user || {}
+        })
+      )
+
+  const createTestConfig = (): End2EndTestConfig => ({
+    setupApplication: (app, services) =>
+      app.use(createUserEchoModule(services)),
+  })
+
+  const makeEchoUserRequest = (
+    cookie: string,
+    server: End2EndTestContext['server']
+  ) =>
+    request(server)
+      .get('/echo-user')
+      .set({
+        cookie: `haffa-token=${cookie}`,
+      })
+})
