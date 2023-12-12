e25eea9cb9a733ecd63d34d1a26e58e15aba1283
Petter Andersson <petter.andersson@helsingborg.se>
2023-12-12 15:15:39 +0100

    fixing an issue where multiple entries to crontab

--- docker-cmd-with-crond.sh
+++ docker-cmd-with-crond.sh
@@ -1,8 +1,14 @@
 # Start crond
-crond
+CROND=`ps -ef -o args | egrep "^crond$" | wc -l`
 
+if [ ${CROND} -eq 0 ]; then
+  echo "Starting crond"
+  crond
+else
+  echo "crond already running"
+fi
 # Update crontab
-crontab -l | { cat; echo "0 23 * * * /usr/src/app/data/jobs/daily.sh>/tmp/daily.log"; } | crontab -
+crontab -l | grep -v "daily.sh" | { cat; echo "0 23 * * * /usr/src/app/data/jobs/daily.sh>/tmp/daily.log"; } | crontab -
 
 # Run application
 node ./index.js
