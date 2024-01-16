bd00341307252ab4145eef930e337aad63a43240
Petter Andersson <petter.andersson@helsingborg.se>
2024-01-16 14:23:02 +0100

    additional unittests for field-config

--- src/advert-field-config/mappers.spec.ts
+++ src/advert-field-config/mappers.spec.ts
@@ -1,21 +1,12 @@
-import { normalizeFieldConfig } from './mappers'
-import type { FieldName } from './types'
-import { FieldLabels } from './types'
-
-const getFieldConfig = (name: FieldName) => ({
-  name,
-  label: FieldLabels[name],
-  visible: true,
-  mandatory: false,
-  initial: '',
-  adornment: '',
-})
+import { getFieldConfig, normalizeFieldConfig } from './mappers'
 
 it('should override default values with input', () => {
   const output = normalizeFieldConfig([
     { ...getFieldConfig('tags'), visible: false, mandatory: true },
     { ...getFieldConfig('material'), visible: false },
     { ...getFieldConfig('width'), mandatory: true },
+    { ...getFieldConfig('weight'), label: 'dummy' }, // Retain labels
+    { ...getFieldConfig('phone'), adornment: 'dummy' }, // Retain adornment
   ])
   expect(output).toEqual([
     { ...getFieldConfig('title') },
@@ -25,7 +16,7 @@ it('should override default values with input', () => {
     { ...getFieldConfig('width'), mandatory: true },
     { ...getFieldConfig('height') },
     { ...getFieldConfig('depth') },
-    { ...getFieldConfig('weight') },
+    { ...getFieldConfig('weight'), label: 'dummy' },
     { ...getFieldConfig('size') },
     { ...getFieldConfig('material'), visible: false },
     { ...getFieldConfig('condition') },
@@ -39,7 +30,7 @@ it('should override default values with input', () => {
     { ...getFieldConfig('zipCode') },
     { ...getFieldConfig('city') },
     { ...getFieldConfig('email') },
-    { ...getFieldConfig('phone') },
+    { ...getFieldConfig('phone'), adornment: 'dummy' },
     { ...getFieldConfig('country') },
   ])
 })
@@ -71,4 +62,46 @@ it('should handle null document', () => {
     { ...getFieldConfig('phone') },
     { ...getFieldConfig('country') },
   ])
+})
+
+it('should remove duplicates (Keep last)', () => {
+  const output = normalizeFieldConfig([
+    getFieldConfig('name'),
+    { ...getFieldConfig('name'), visible: false },
+  ])
+  expect(output).toEqual([
+    { ...getFieldConfig('title') },
+    { ...getFieldConfig('description') },
+    { ...getFieldConfig('quantity') },
+    { ...getFieldConfig('unit') },
+    { ...getFieldConfig('width') },
+    { ...getFieldConfig('height') },
+    { ...getFieldConfig('depth') },
+    { ...getFieldConfig('weight') },
+    { ...getFieldConfig('size') },
+    { ...getFieldConfig('material') },
+    { ...getFieldConfig('condition') },
+    { ...getFieldConfig('usage') },
+    { ...getFieldConfig('category') },
+    { ...getFieldConfig('reference') },
+    { ...getFieldConfig('tags') },
+    { ...getFieldConfig('organization') },
+    { ...getFieldConfig('name'), visible: false },
+    { ...getFieldConfig('adress') },
+    { ...getFieldConfig('zipCode') },
+    { ...getFieldConfig('city') },
+    { ...getFieldConfig('email') },
+    { ...getFieldConfig('phone') },
+    { ...getFieldConfig('country') },
+  ])
+})
+
+it('should provide labels', () => {
+  const output = normalizeFieldConfig(null)
+
+  expect(output).toHaveLength(
+    output.filter(
+      field => typeof field.label === 'string' && field.label.length > 0
+    ).length
+  )
 })
