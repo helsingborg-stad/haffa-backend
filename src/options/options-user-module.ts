8f011463ff160a8378073a9d9927b9308de9c882
Joakim Larsson <mimmertim@gmail.com>
2024-01-19 15:05:20 +0100

    exposing interesting system settings

--- src/options/options-user-module.ts
+++ src/options/options-user-module.ts
@@ -1,14 +1,46 @@
 import type { ApplicationModule } from '@helsingborg-stad/gdi-api-node'
 import type { SettingsService } from '../settings/types'
 import { optionsAdapter } from './options-adapter'
+import { type Option } from './types'
+import { userMapperConfigAdapter } from '../users'
+
+const getSystemSettings = async (
+  settings: SettingsService
+): Promise<Option[]> => {
+  const {
+    allowGuestUsers,
+    phone: { allowPhoneUsers },
+  } = await userMapperConfigAdapter(settings).getUserMapperConfig()
+
+  return [
+    {
+      key: 'allowEmailUsers',
+      value: 'true',
+    },
+    {
+      key: 'allowGuestUsers',
+      value: allowGuestUsers ? 'true' : 'false',
+    },
+    {
+      key: 'allowPhoneUsers',
+      value: allowPhoneUsers ? 'true' : 'false',
+    },
+  ]
+}
 
 export const optionsUserModule =
   (settings: SettingsService): ApplicationModule =>
   ({ registerKoaApi }) =>
     registerKoaApi({
+      systemSettings: async ctx => {
+        ctx.response.body = await getSystemSettings(settings)
+      },
       options: async ctx => {
+        const {
+          params: { collection },
+        } = ctx
         ctx.response.body = await optionsAdapter(settings).getOptions(
-          ctx.params.collection
+          collection
         )
       },
     })
