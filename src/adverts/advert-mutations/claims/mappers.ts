a6a78e8177b03227bd6796d90d5bed15f28e05f0
Petter Andersson <petter.andersson@helsingborg.se>
2024-02-08 16:26:17 +0100

    documentation

--- src/adverts/advert-mutations/claims/mappers.ts
+++ src/adverts/advert-mutations/claims/mappers.ts
@@ -1,9 +1,20 @@
 import { type AdvertClaim, type AdvertClaimEvent } from '../../types'
 
+const sanitizeInterval = (interval: number) => (interval < 0 ? 0 : interval)
+/**
+ * Returns the date when the last event was generated
+ * e.g when the last reminder was sent
+ * @param claim A refereence to an AdvertClaim
+ * @returns A date object
+ */
 export const getLastClaimEventDate = (claim: AdvertClaim): Date => {
+  // Sort by date callback
   const e = (a: AdvertClaimEvent, b: AdvertClaimEvent) =>
     b.at.localeCompare(a.at)
 
+  // If no events are logged, return the date
+  // of the claim itself as it represents the
+  // time of the latest status change
   return new Date(
     claim.events && claim.events.length > 0
       ? [...claim.events].sort(e)[0].at
@@ -11,11 +22,26 @@ export const getLastClaimEventDate = (claim: AdvertClaim): Date => {
   )
 }
 
+/**
+ * Given an interval, calculate when, at the earliest
+ * the next event is allowed to be logged
+ * @param claim A reference to an AdvertClaim
+ * @param interval The number of days that should occur before a new event is logged
+ * @returns A Date object
+ */
 export const getNextClaimEventDate = (claim: AdvertClaim, interval: number) => {
   const d = getLastClaimEventDate(claim)
-  return new Date(d.setDate(d.getDate() + interval))
+  return new Date(d.setDate(d.getDate() + sanitizeInterval(interval)))
 }
 
+/**
+ * Calculates the status of a claim given that it has a limited validity
+ *
+ * @param claim A reference to an AdvertClaim
+ * @param daysValid The number of days from the latest status change the claim is valid
+ * @param now The current date and time to validate towards
+ * @returns true if the claim is invalid, otherwise false
+ */
 export const isClaimOverdue = (
   claim: AdvertClaim,
   daysValid: number,
@@ -23,8 +49,9 @@ export const isClaimOverdue = (
 ) => {
   if (daysValid > 0) {
     const expireDate = new Date(claim.at)
-    expireDate.setDate(expireDate.getDate() + daysValid)
+    expireDate.setDate(expireDate.getDate() + sanitizeInterval(daysValid))
     return now > expireDate
   }
+  // No limitation on validity
   return false
 }
