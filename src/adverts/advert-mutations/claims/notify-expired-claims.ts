10ed5f0a37c48b8bcdf6b938e54b8f502afeb908
Petter Andersson <petter.andersson@helsingborg.se>
2025-03-04 14:46:21 +0100

    expire reservations based on pickedAt

--- src/adverts/advert-mutations/claims/notify-expired-claims.ts
+++ src/adverts/advert-mutations/claims/notify-expired-claims.ts
@@ -1,3 +1,4 @@
+import { sortBy } from '../../../lib'
 import { Severity } from '../../../syslog/types'
 import { txBuilder } from '../../../transactions'
 import type { Services } from '../../../types'
@@ -28,79 +29,77 @@ export const createExpiredClaimsNotifier =
       .validate(() => undefined)
       .patch((advert, { actions }) => {
         const meta = getAdvertMeta(advert, user, now)
-        // =====================================
+
         // Rule: Supress notification
-        // =====================================
-        // Apply when:
-        // 1. Advert is not yet picked
-        // AND
-        // 2. reminderSnoozeUntilPicked is set to 1
-        //
         if (snooze === 1 && !meta.isPicked) {
           return null
         }
 
-        const claims = advert.claims.reduce<AdvertClaim[]>((p, c) => {
-          // =====================================
-          // Rule: Determine comparison date
-          // =====================================
-          // Comparison date will be pickedAt when
-          // 1. Advert is picked
-          // AND
-          // 2. pickedAt is later than claim date
-          // AND
-          // 3. reminderSnoozeUntilPicked is set to 1
-          //
-          // Otherwise comparison date will be claim date
-          //
-          const defaultDate =
-            snooze === 1 && meta.isPicked
-              ? [c.at, advert.pickedAt ?? '']
-                  .filter(x => x !== '')
-                  .sort((x, y) => y.localeCompare(x))[0]
-              : c.at
+        const claimsWithMeta = advert.claims
+          // Tag reserved claims
+          .map<{
+            claim: AdvertClaim
+            isReserved: boolean
+          }>(claim => ({
+            claim,
+            isReserved: claim.type === AdvertClaimType.reserved,
+          }))
+          // Determine if notification should take place
+          .map<{
+            claim: AdvertClaim
+            shouldNotify: boolean
+          }>(({ claim, isReserved }) => {
+            const defaultDate =
+              snooze === 1 && meta.isPicked
+                ? [claim.at, advert.pickedAt ?? '']
+                    .filter(x => x !== '')
+                    .sort(sortBy(x => x))
+                    .reverse()[0]
+                : claim.at
 
-          if (
-            // Check the claim reservation status
-            // =====================================
-            // A) Claim is of type "reserved"
-            // B) Claim creation date + max reservations is larger than "now"
-            c.type === AdvertClaimType.reserved &&
-            isClaimOverdue(c, interval, now, defaultDate)
-          ) {
-            // Queue notification for Email/SMS delivery
-            actions(() =>
-              notifications.advertReservationWasCancelled(
-                c.by,
-                user,
-                c.quantity,
-                advert,
-                null
+            return {
+              claim,
+              shouldNotify:
+                isReserved && isClaimOverdue(claim, interval, now, defaultDate),
+            }
+          })
+
+        const claims = claimsWithMeta.reduce<AdvertClaim[]>(
+          (p, { claim, shouldNotify }) => {
+            if (shouldNotify) {
+              // Queue notification for Email/SMS delivery
+              actions(() =>
+                notifications.advertReservationWasCancelled(
+                  claim.by,
+                  user,
+                  claim.quantity,
+                  advert,
+                  null
+                )
               )
-            )
-            actions(() =>
-              notifications.advertReservationWasCancelledOwner(
-                advert.createdBy,
-                user,
-                c.quantity,
-                advert,
-                null
+              actions(() =>
+                notifications.advertReservationWasCancelledOwner(
+                  advert.createdBy,
+                  user,
+                  claim.quantity,
+                  advert,
+                  null
+                )
               )
-            )
-            actions(() =>
-              syslog.write({
-                by: user.id,
-                type: 'NOTIFY_EXPIRED_CLAIM',
-                severity: Severity.info,
-                message: `Removed claim for: ${c.by} on ${advert.id}`,
-              })
-            )
-            // Remove claim from advert
-            return [...p]
-          }
-          // Retain claim in advert
-          return [...p, c]
-        }, [])
+              actions(() =>
+                syslog.write({
+                  by: user.id,
+                  type: 'NOTIFY_EXPIRED_CLAIM',
+                  severity: Severity.info,
+                  message: `Removed claim for: ${claim.by} on ${advert.id}`,
+                })
+              )
+              return [...p]
+            }
+            return [...p, claim]
+          },
+          []
+        )
 
         // One or more claims has been removed
         if (claims.length !== advert.claims.length) {
--- src/adverts/advert-mutations/claims/notify-reserved-claims.ts
+++ src/adverts/advert-mutations/claims/notify-reserved-claims.ts
@@ -1,13 +1,10 @@
+import { sortBy } from '../../../lib'
 import { Severity } from '../../../syslog/types'
 import { txBuilder } from '../../../transactions'
 import type { Services } from '../../../types'
 import { normalizeAdvertClaims } from '../../advert-claims'
-import {
-  type Advert,
-  type AdvertMutations,
-  AdvertClaimEventType,
-  AdvertClaimType,
-} from '../../types'
+import { AdvertClaimEventType, AdvertClaimType } from '../../types'
+import type { AdvertClaim, Advert, AdvertMutations } from '../../types'
 import { mapTxResultToAdvertMutationResult } from '../mappers'
 import { getNextClaimEventDate } from './mappers'
 
@@ -27,82 +24,85 @@ export const createReservedClaimsNotifier =
       .validate(() => undefined)
       .patch((advert, { actions }) => {
         const meta = getAdvertMeta(advert, user, now)
-        // =====================================
+
         // Rule: Supress notification
-        // =====================================
-        // Apply when:
-        // 1. Advert is not yet picked
-        // AND
-        // 2. reminderSnoozeUntilPicked is set to 1
         if (snooze === 1 && !meta.isPicked) {
           return null
         }
 
-        let isModified = false
-        const claims = advert.claims.map(c => {
-          // =====================================
-          // Rule: Determine comparison date
-          // =====================================
-          // Comparison date will be pickedAt when
-          // 1. Advert is picked
-          // AND
-          // 2. pickedAt is later than claim date
-          // AND
-          // 3. reminderSnoozeUntilPicked is set to 1
-          //
-          // Otherwise comparison date will be claim date
-          //
-          const defaultDate =
-            snooze === 1 && meta.isPicked
-              ? [c.at, advert.pickedAt ?? '']
-                  .filter(x => x !== '')
-                  .sort((x, y) => y.localeCompare(x))[0]
-              : c.at
-          // =====================================
-          // Rule: Check the claim reserved status
-          // =====================================
-          // A) Claim is of type "reserved"
-          // B) Determine the last time a reminder was sent
-          //
-          // defaultDate will either be the claim date or the pickedAt date
-          // of the advert.
+        const claims = advert.claims
+          // Tag reserved claims
+          .map<{
+            claim: AdvertClaim
+            isReserved: boolean
+          }>(claim => ({
+            claim,
+            isReserved: claim.type === AdvertClaimType.reserved,
+          }))
+          // Determine if notification should take place
+          .map<{
+            claim: AdvertClaim
+            shouldNotify: boolean
+          }>(({ claim, isReserved }) => {
+            const defaultDate =
+              snooze === 1 && meta.isPicked
+                ? [claim.at, advert.pickedAt ?? '']
+                    .filter(x => x !== '')
+                    .sort(sortBy(x => x))
+                    .reverse()[0]
+                : claim.at
 
-          if (
-            c.type === AdvertClaimType.reserved &&
-            now >= getNextClaimEventDate(c, interval, defaultDate)
-          ) {
-            // Queue notification for Email/SMS delivery
+            return {
+              claim,
+              shouldNotify:
+                isReserved &&
+                now >= getNextClaimEventDate(claim, interval, defaultDate),
+            }
+          })
+          // Write Event
+          .map(({ claim, shouldNotify }) =>
+            shouldNotify
+              ? {
+                  shouldNotify,
+                  claim: {
+                    ...claim,
+                    events: [
+                      ...claim.events,
+                      {
+                        type: AdvertClaimEventType.reminder,
+                        at: now.toISOString(),
+                      },
+                    ],
+                  },
+                }
+              : { shouldNotify, claim }
+          )
+        const isModified = claims
+          .filter(claim => claim.shouldNotify)
+          .reduce<boolean>((_, { claim }) => {
             actions(() =>
-              notifications.advertNotCollected(c.by, user, c.quantity, advert)
+              notifications.advertNotCollected(
+                claim.by,
+                user,
+                claim.quantity,
+                advert
+              )
             )
             actions(() =>
               syslog.write({
                 by: user.id,
                 type: 'NOTIFY_RESERVATION_REMINDER',
                 severity: Severity.info,
-                message: `Reminder sent to: ${c.by} on ${advert.id}`,
+                message: `Reminder sent to: ${claim.by} on ${advert.id}`,
               })
             )
-            isModified = true
-            // Add reminder event
-            return {
-              ...c,
-              events: [
-                ...c.events,
-                {
-                  type: AdvertClaimEventType.reminder,
-                  at: now.toISOString(),
-                },
-              ],
-            }
-          }
-          return c
-        })
+            return true
+          }, false)
         // One or more claims has been removed
         return isModified
           ? {
               ...advert,
-              claims: normalizeAdvertClaims(claims),
+              claims: normalizeAdvertClaims(claims.map(({ claim }) => claim)),
             }
           : null
       })
