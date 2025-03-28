bcd651b979bcae6d7d3d77219214a3fe9caf3151
Joakim Larsson <mimmertim@gmail.com>
2025-03-28 10:40:14 +0100

    updated pick notifications logic

--- src/adverts/advert-mutations/notifications/advert-notifier.ts
+++ src/adverts/advert-mutations/notifications/advert-notifier.ts
@@ -2,8 +2,15 @@ import type { Func } from '../../../lib/types'
 import type { HaffaUser } from '../../../login/types'
 import type { NotificationService } from '../../../notifications/types'
 import { patchAdvertWithPickupLocation } from '../../../pickup/mappers'
+import type { AdvertClaim } from '../../types'
 import { AdvertClaimType } from '../../types'
 import type { AdvertNotifier } from './types'
+
+const isReservedClaim = ({ type }: AdvertClaim) =>
+  type === AdvertClaimType.reserved
+
+const ensureAtleastOne = <T>(l: T[], theOne: T): T[] =>
+  l.length > 0 ? l : [theOne]
 
 export const createAdvertNotifier: Func<
   {
@@ -20,9 +27,22 @@ export const createAdvertNotifier: Func<
     notifications.advertWasRemoved(advert.createdBy, user, advert),
   wasPicked: (advert, claims) =>
     Promise.all([
-      notifications.advertWasPickedOwner(advert.createdBy, user, advert),
+      // get reservation pickup locations
+      ...ensureAtleastOne(
+        claims
+          .filter(isReservedClaim)
+          .map(({ pickupLocation }) => pickupLocation)
+          .filter(pickupLocation => pickupLocation),
+        undefined // ...or a default unset one
+      ).map(pickupLocation =>
+        notifications.advertWasPickedOwner(
+          advert.createdBy,
+          user,
+          patchAdvertWithPickupLocation(advert, pickupLocation)
+        )
+      ),
       ...claims
-        .filter(({ type }) => type === AdvertClaimType.reserved)
+        .filter(isReservedClaim)
         .map(({ by: to, pickupLocation }) =>
           notifications.advertWasPicked(
             to,
