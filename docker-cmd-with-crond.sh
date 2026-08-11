0688e6a4389c225ebcc1e4011ba96fbb076f333a
Petter Andersson <40427478+petter-a@users.noreply.github.com>
2026-08-11 10:28:39 +0200

    Add npm install for sharp package
    Change directory to /usr/src/app and install sharp package.

--- docker-cmd-with-crond.sh
+++ docker-cmd-with-crond.sh
@@ -10,5 +10,8 @@ fi
 # Update crontab
 crontab -l | grep -v "daily.sh" | { cat; echo "0 06 * * * /usr/src/app/data/jobs/daily.sh>/tmp/daily.log"; } | crontab -
 
+cd /usr/src/app
+npm install --platform=linuxmusl --arch=x64 sharp
+
 # Run application
 node ./index.js
